c5aea80ea5a40451ea589669767090d401aa3c72
priyashR <priyash.ramawthar@gmail.com>
2019-05-06 17:55:21 +0200

    added parsing logic

--- budgetDataIngestion/src/main/java/com/gmail/ramawthar/priyash/BudgetTrxns/BudgetTransactions.java
+++ budgetDataIngestion/src/main/java/com/gmail/ramawthar/priyash/BudgetTrxns/BudgetTransactions.java
@@ -71,7 +71,7 @@ public class BudgetTransactions implements ProcessEmail{
 	}
 	
 	private void parseLine(String unprocessedLine){
-		
+		/* -> Processing using OpenFaas - the logic has been imported
 		final String serverIP = "172.16.0.128";//do an ipconfig to get the host ip value
 		//final String serverIP = "127.0.0.1";
 		String lineResult = "";
@@ -96,14 +96,97 @@ public class BudgetTransactions implements ProcessEmail{
         result = result + " " + lineResult;
         System.out.println("lineResult: "+lineResult);
         System.out.println("result: "+result);
-        
-        pushToQueue(lineResult);
+        */
+		
+        String input = "{\"transaction\":\""+unprocessedLine.replace("\n", "").replace("\r", "").replace(";", "-")+"\"}";
+        System.out.println("input: "+input);
+        pushToQueue(parsingLogic(input));
 	}
+	
+
+
+	private String parsingLogic(String transaction){
+		System.out.println("transaction: "+transaction);
+		
+		String trxn = transaction;
+
+        //logic to strip info
+
+		if (trxn.contains(" reserved for purchase @ ")){		
+			trxn = trxn.substring(trxn.indexOf("FNB :-) R")+9);
+	
+			trxn = trxn.replace(" reserved for purchase @ ", ";")
+					   .replace(" from FNB card a/c..", ";")
+					   .replace(" using card..", ";")
+					   .replace(". Avail R", ";")
+					   .replace(". ", ";");
+					
+			trxn = "creditPurchase;out;"+(trxn.substring(0, trxn.length()-6))+";"+trxn.substring(trxn.length()-5);
+			
+		} else if(trxn.contains(" paid from cheq a/c")){
+			trxn = trxn.substring(trxn.indexOf("FNB :-) R")+9);
+			
+			trxn = trxn.replace(" paid from cheq a/c..", ";")
+					   .replace(" @ Online Banking. Avail R", ";")
+					   .replace(" @ Eft. Avail R", ";")
+					   .replace(". Ref.", ";")
+					   .replace(". ", ";");
+			
+			trxn = "cheqPurchase;out;"+(trxn.substring(0, trxn.length()-6))+";"+trxn.substring(trxn.length()-5);
+			
+		} else if(trxn.contains(" paid to cheq a/c..")){
+			trxn = trxn.substring(trxn.indexOf("FNB :-) R")+9);
+			
+			
+			trxn = trxn.replace(" paid to cheq a/c..", ";")
+					   .replace(" @ Eft. Ref.", ";")
+					   .replace(". ", ";");
+			
+			trxn = "cheqDeposits;in;"+(trxn.substring(0, trxn.length()-6))+";"+trxn.substring(trxn.length()-5);
+			
+		} else if(trxn.contains(" withdrawn from cheq a/c..")){
+			trxn = trxn.substring(trxn.indexOf("FNB :-) R")+9);
+			
+			trxn = trxn.replace(" withdrawn from cheq a/c..", ";")
+					   .replace(" @ ", ";")
+					   .replace(". Avail R", ";")
+					   .replace(". ", ";");
+				
+			trxn = "cheqWithdrawal;out;"+(trxn.substring(0, trxn.length()-6))+";"+trxn.substring(trxn.length()-5);
+			
+		} else if(trxn.contains(" t/fer from cheq a/c..")){
+			trxn = trxn.substring(trxn.indexOf("FNB :-) R")+9);
+			
+			trxn = trxn.replace(" t/fer from cheq a/c..", ";")
+					   .replace(" to Notice a/c..", ";")
+					   .replace(" @ Online Banking. Avail R", ";")
+					   .replace(". ", ";");
+					
+			trxn = "cheqTransfer;both;"+(trxn.substring(0, trxn.length()-6))+";"+trxn.substring(trxn.length()-5);
+			
+			
+		}
+
+        //logic done.
+		
+		return trxn;
+	}
+	
 	private void pushToQueue(String processedLine){
 		//works!
-		
+		/* removed for testing - place put back
 		QueueManager qm = new QueueManager();
-		qm.publishToQueue(processedLine);
+		qm.publishToQueue(processedLine);*/
+		System.out.println("processedLine  : "+processedLine);
+		System.out.println("complete  :) ");
 	
+	}
+	
+	public static void main(String [] args){
+		System.out.println("hi");
+		BudgetTransactions bt = new BudgetTransactions("test body");
+		String unprocessedLine = "• FNB :-) R122.06 reserved for purchase @ Checkers Leaping Frog from FNB card a/c..947000 using card..7046. Avail R17811. 4May 11:24";
+		bt.parseLine(unprocessedLine);
+		
 	}
 }
